74429fce6bb1068e267f05fe77779f2d7c55959c
Axel Nyffenegger <Axnyff@gmail.com>
2025-06-03 18:02:38 +0200

    Wip

--- Parser.js
+++ Parser.js
@@ -1,25 +1,46 @@
-const parseNumber = (input) => {
-  const sign = input[0] === "	" ? -1 : 1;
-
-};
-
 class Parser {
-  constructor(input) {
-    this.input = input.replace(/\S/g, "");
+  constructor(program, input) {
+    this.program = program.replace(/\S/g, "");
     this.position = 0;
     this.stack = [];
+    this.heap = {};
+    this.output = "";
+    this.input = input;
+    this.labels = {};
+    this.calls = [];
+  }
+
+  forceStack(stack) {
+    this.stack = stack;
+  }
+
+
+  forceHeap(heap) {
+    this.heap = heap;
+  }
+
+  getOutput() {
+    return this.output;
+  }
+
+  getStack() {
+    return this.stack;
+  }
+
+  getHeap() {
+    return this.heap;
   }
 
   peek() {
-    return this.input[this.position];
+    return this.program[this.position];
   }
 
   peekNext() {
-    return this.input[this.position + 1];
+    return this.program[this.position + 1];
   }
 
   consume() {
-    return this.input[this.position++];
+    return this.program[this.position++];
   }
 
   parseNumber() {
@@ -32,6 +53,7 @@ class Parser {
     while (this.peek() !== "\n") {
       s += this.consume() === " " ? "0" : "1";
     }
+    this.consume();
     if (!s) {
       return 0;
     }
@@ -50,39 +72,272 @@ class Parser {
   parseStackCommand() {
     this.consume();
     if (this.peek() === " ") {
-      console.log("PUSH");
       this.consume();
       this.stack.push(this.parseNumber());
+      return;
     }
     if (this.peek() === "\t" && this.peekNext() === " ") {
-      console.log("DUP");
       this.consume();
       this.consume();
       let nth = this.parseNumber();
-      this.stack.push(this.stack.at(-nth));
+      this.stack.push(this.stack[this.stack.length - 1 - nth]);
+      return;
     }
-
+    if (this.peek() === "\t" && this.peekNext() === "\n") {
+      this.consume();
+      this.consume();
+      let count = this.parseNumber();
+      if (count < 0 || count >= this.stack.length) {
+        this.stack = [this.stack[this.stack.length - 1]];
+      }
+      const last = this.stack.pop();
+      for (let i = 0; i < count; i++) {
+        this.stack.pop();
+      }
+      this.stack.push(last);
+      return;
+    }
+    if (this.peek() === "\n" && this.peekNext() === " ") {
+      this.consume();
+      this.consume();
+      this.stack.push(this.stack[this.stack.length - 1]);
+      return;
+    }
+    if (this.peek() === "\n" && this.peekNext() === "\t") {
+      this.consume();
+      this.consume();
+      const top = this.stack.pop();
+      const buttop = this.stack.pop();
+      if (buttop === undefined) {
+        throw new Error();
+      }
+      this.stack.push(top);
+      this.stack.push(buttop);
+      return;
+    }
+    if (this.peek() === "\n" && this.peekNext() === "\n") {
+      this.consume();
+      this.consume();
+      const popped = this.stack.pop();
+      if (popped === undefined) {
+        throw new Error();
+      }
+      return;
+    }
   }
+
+  parseArithmeticCommand() {
+    this.consume();
+    this.consume();
+    if (this.peek() === " " && this.peekNext() === " ") {
+      this.consume();
+      this.consume();
+      const a = this.stack.pop();
+      const b = this.stack.pop();
+      this.stack.push(a + b);
+      return;
+    }
+    if (this.peek() === " " && this.peekNext() === "\t") {
+      this.consume();
+      this.consume();
+      const a = this.stack.pop();
+      const b = this.stack.pop();
+      this.stack.push(b-a);
+      return;
+    }
+    if (this.peek() === " " && this.peekNext() === "\n") {
+      this.consume();
+      this.consume();
+      const a = this.stack.pop();
+      const b = this.stack.pop();
+      this.stack.push(b*a);
+      return;
+    }
+    if (this.peek() === "\t" && this.peekNext() === " ") {
+      this.consume();
+      this.consume();
+      const a = this.stack.pop();
+      const b = this.stack.pop();
+      if (a === 0) {
+        throw new Error("Division par zero")
+      }
+      this.stack.push(Math.floor(b/a));
+      return;
+    }
+    if (this.peek() === "\t" && this.peekNext() === "\t") {
+      this.consume();
+      this.consume();
+      const a = this.stack.pop();
+      const b = this.stack.pop();
+      if (a === 0) {
+        throw new Error("Modulo par zero")
+      }
+      this.stack.push(Math.floor(b%a));
+      return;
+    }
+  }
+
+  parseHeapCommand() {
+    this.consume();
+    this.consume();
+    if (this.peek() === " ") {
+      this.consume();
+      const a = this.stack.pop();
+      const b = this.stack.pop();
+
+      this.heap[b] = a;
+      return;
+    }
+    if (this.peek() === "\t") {
+      this.consume();
+      const a = this.stack.pop();
+      this.stack.push(this.heap[a]);
+      return;
+    }
+  }
+
+  parseIOCommand() {
+    this.consume();
+    this.consume();
+
+    if (this.peek() === " " && this.peekNext() === " ") {
+      this.consume();
+      this.consume();
+
+      this.output += String.fromCharCode(this.stack.pop());
+      return;
+    }
+    if (this.peek() === " " && this.peekNext() === "\t") {
+      this.consume();
+      this.consume();
+
+      this.output += this.stack.pop();
+      return;
+    }
+    if (this.peek() === "\t" && this.peekNext() === " ") {
+      this.consume();
+      this.consume();
+
+      const a = this.input.charCodeAt(0);
+      this.input = this.input.slice(1);
+      const b = this.stack.pop();
+      this.heap[b] = a;
+      return;
+    }
+    if (this.peek() === "\t" && this.peekNext() === "\t") {
+      this.consume();
+      this.consume();
+
+      let i = 0;
+      while (this.input[i] !== "\n") {
+        i++;
+        if (!this.input[i]) {
+          throw new Error("Can't read number");
+        }
+      }
+      const b = this.stack.pop();
+      if (this.input.startsWith("0x")) {
+        const a = parseInt(this.input.slice(2), 16);
+        this.heap[b] = a;
+      } else {
+        const a = parseInt(this.input, 10);
+        this.heap[b] = a;
+      }
+      this.input = this.input.slice(i);
+      return;
+    }
+  }
+
+  parseFlow() {
+    this.consume();
+    if (this.peek() === " " && this.peekNext() === " ") {
+      this.consume();
+      this.consume();
+      const label = this.parseLabel();
+      this.labels[label] = this.position;
+      return;
+    }
+    if (this.peek() === " " && this.peekNext() === "\t") {
+      this.consume();
+      this.consume();
+      const label = this.parseLabel();
+      this.calls.push(this.position);
+      this.position = this.labels[label];
+      return;
+    }
+    if (this.peek() === " " && this.peekNext() === "\n") {
+      this.consume();
+      this.consume();
+      const label = this.parseLabel();
+      this.position = this.labels[label];
+      return;
+    }
+    if (this.peek() === "\t" && this.peekNext() === " ") {
+      this.consume();
+      this.consume();
+      const label = this.parseLabel();
+      if (this.stack.pop() === 0) {
+        this.position = this.labels[label];
+      }
+      return;
+    }
+    if (this.peek() === "\t" && this.peekNext() === "\t") {
+      this.consume();
+      this.consume();
+      const label = this.parseLabel();
+      if (this.stack.pop() < 0) {
+        this.position = this.labels[label];
+      }
+      return;
+    }
+    if (this.peek() === "\t" && this.peekNext() === "\n") {
+      this.consume();
+      this.consume();
+      this.position = this.calls.pop();
+      return;
+    }
+    if (this.peek() === "\n" && this.peekNext() === "\n") {
+      this.consume();
+      this.consume();
+      this.exit = true;
+      return;
+    }
+  }
+
 
   parseCommand() {
     if (this.peek() === " ") {
       return this.parseStackCommand();
     }
+    if (this.peek() === "\t" && this.peekNext() === " ") {
+      return this.parseArithmeticCommand();
+    }
+    if (this.peek() === "\t" && this.peekNext() === "\t") {
+      return this.parseHeapCommand();
+    }
+    if (this.peek() === "\t" && this.peekNext() === "\n") {
+      return this.parseIOCommand();
+    }
+    if (this.peek() === "\n") {
+      return this.parseFlow();
+    }
   }
 
   parseProgram() {
-    this.parseCommand();
+    while (!this.exit && this.peek()) {
+      this.parseCommand();
+    }
+    if (!this.peek() && !this.exit) {
+      throw new Error("oo");
+    }
+    return this.output;
   }
-
-  printStack() {
-    console.log("STACK:")
-    console.log(this.stack.join("\n"));
-  }
-
 }
 
-const runWhitespace = (raw_input) => {
-  const input = raw_input.replace(/\S/g, "");
+const whitespace = (code, input) => {
+  const true_code = code.replace(/\S/g, "");
+  return new Parser(true_code, input).parseProgram();
 };
 
 module.exports = Parser;
+module.exports.whitespace = whitespace;
--- test.js
+++ test.js
@@ -1,10 +1,273 @@
 const Parser = require("./Parser");
 
+describe("Parser.parseNumber", () => {
+  it("should parse 0", () => {
+    expect(new Parser(`\t\n'`).parseNumber()).toBe(0);
+  });
+  it("should parse 2", () => {
+    expect(new Parser(` \t \n'`).parseNumber()).toBe(2);
+  });
+  it("should parse -5", () => {
+    expect(new Parser(`\t\t \t\n'`).parseNumber()).toBe(-5);
+  });
+  it("should throw if terminal", () => {
+    expect(() => new Parser(`\n'`).parseNumber()).toThrow();
+  });
+});
+describe("Parser.parseLabel", () => {
+  it("should parse labels", () => {
+    expect(new Parser(`\t\n'`).parseLabel()).toBe("\t");
+    expect(new Parser(`\n'`).parseLabel()).toBe("");
+    expect(new Parser(`\t\t\t  \n'`).parseLabel()).toBe("\t\t\t  ");
+  });
+});
 
-console.assert(new Parser(`\t\n'`).parseNumber() === 0, "Should parse number 0");
-console.assert(new Parser(`\t\t \n'`).parseNumber() === 2, "Should parse number");
+describe("Parser.parseCommand (stack)", () => {
+  it("should handle a single stack command", () => {
+    const parser = new Parser(`   \t\t\n`);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([3]);
+  });
+  it("should handle multiple stack commands", () => {
+    const parser = new Parser(`   \t\t\n   \t \t\n`);
+    parser.parseCommand();
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([3, 5]);
+  });
+  it("should handle duplicating the last item", () => {
+    const parser = new Parser(` \t \t\n`);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 6, 7, 7]);
+  });
+  it("should handle popping everything", () => {
+    const parser = new Parser(` \t\n\t\t\n`);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([7]);
+  });
+  it("should handle popping some items", () => {
+    const parser = new Parser(` \t\n \t \n`);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 7]);
+  });
+  it("should handle duplicating top value", () => {
+    const parser = new Parser(` \n `);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 6, 7, 7]);
+  });
+  it("should handle swapping the top values", () => {
+    const parser = new Parser(` \n\t`);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 7, 6]);
+  });
+  it("should handle popping the top value", () => {
+    const parser = new Parser(` \n\n`);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+});
+describe("Parser.parseCommand (arithmetic)", () => {
+  it("should handle an addition", () => {
+    const parser = new Parser(`\t   `);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 13]);
+  });
+  it("should handle a addition", () => {
+    const parser = new Parser(`\t  \t`);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, -1]);
+  });
+  it("should handle a multiplication", () => {
+    const parser = new Parser(`\t  \n`);
+    parser.forceStack([1, 2, 3, 4, 5, 6, 7]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 42]);
+  });
+  it("should handle a division (rounded)", () => {
+    const parser = new Parser(`\t \t `);
+    parser.forceStack([1, 2, 3, 4, 5, 27, 4]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+  it("should throw if zero division", () => {
+    const parser = new Parser(`\t \t `);
+    parser.forceStack([1, 2, 3, 4, 5, 5, 0]);
+    expect(() => parser.parseCommand()).toThrow();
+  });
+  it("should handle a modulo", () => {
+    const parser = new Parser(`\t \t\t`);
+    parser.forceStack([1, 2, 3, 4, 5, 27, 4]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 2, 3, 4, 5, 3]);
+  });
+  it("should throw if zero division", () => {
+    const parser = new Parser(`\t \t\t`);
+    parser.forceStack([1, 2, 3, 4, 5, 5, 0]);
+    expect(() => parser.parseCommand()).toThrow();
+  });
+});
 
-const parser = new Parser(`   \t\n  \t\n`)
-parser.parseProgram();
-parser.parseProgram();
-parser.printStack();
+describe("Parser.parseCommand (head)", () => {
+  it("should handle adding to the heap from the stack", () => {
+    const parser = new Parser(`\t\t `);
+    parser.forceStack([1, 2]);
+    parser.parseCommand();
+    expect(parser.getHeap()).toEqual({ 1: 2 });
+  });
+  it("should handle adding to the stack from the heap", () => {
+    const parser = new Parser(`\t\t\t`);
+    parser.forceStack([1, 2]);
+    parser.forceHeap({ 2: 3 });
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([1, 3]);
+  });
+});
+
+describe("Parser.parseCommand (i/o)", () => {
+  it("should handle adding a char to the output", () => {
+    const parser = new Parser(`\t\n  `);
+    parser.forceStack([97]);
+    parser.parseCommand();
+    expect(parser.getOutput()).toEqual("a");
+  });
+  it("should handle adding a number to the output", () => {
+    const parser = new Parser(`\t\n \t`);
+    parser.forceStack([97]);
+    parser.parseCommand();
+    expect(parser.getOutput()).toEqual("97");
+  });
+  it("should handle adding a value to the stack from the input", () => {
+    const parser = new Parser(`\t\n\t `, "a");
+    parser.forceStack([97]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([]);
+    expect(parser.getHeap()).toEqual({ 97: 97 });
+  });
+  it("should handle adding a number to the stack from the input", () => {
+    const parser = new Parser(`\t\n\t\t`, "10\n");
+    parser.forceStack([97]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([]);
+    expect(parser.getHeap()).toEqual({ 97: 10 });
+  });
+  it("should handle adding a number (hexa) to the stack from the input", () => {
+    const parser = new Parser(`\t\n\t\t`, "0x10\n");
+    parser.forceStack([97]);
+    parser.parseCommand();
+    expect(parser.getStack()).toEqual([]);
+    expect(parser.getHeap()).toEqual({ 97: 16 });
+  });
+});
+
+describe("Parser.parseCommand (flow control)", () => {
+  it("should handle creating a label", () => {
+    const parser = new Parser(`\n  \t\t\n`);
+    parser.parseCommand();
+    expect(parser.labels).toEqual({ "\t\t": 6 });
+  });
+  it("should handle going to a subroutine", () => {
+    const parser = new Parser(`\n \t\t\t\n`);
+    parser.labels = { "\t\t": 130 };
+
+    parser.parseCommand();
+    expect(parser.position).toBe(130);
+    expect(parser.calls).toEqual([6]);
+  });
+  it("should handle going to a label", () => {
+    const parser = new Parser(`\n \n\t\t\n`);
+    parser.labels = { "\t\t": 130 };
+
+    parser.parseCommand();
+    expect(parser.position).toBe(130);
+    expect(parser.calls).toEqual([]);
+  });
+  it("should handle going to a label if zero", () => {
+    let parser = new Parser(`\n\t \t\t\n`);
+    parser.stack = [4];
+    parser.labels = { "\t\t": 130 };
+
+    parser.parseCommand();
+    expect(parser.position).toBe(6);
+    expect(parser.calls).toEqual([]);
+    expect(parser.stack).toEqual([]);
+
+    parser = new Parser(`\n\t \t\t\n`);
+    parser.stack = [0];
+    parser.labels = { "\t\t": 130 };
+
+    parser.parseCommand();
+    expect(parser.position).toBe(130);
+    expect(parser.calls).toEqual([]);
+    expect(parser.stack).toEqual([]);
+  });
+  it("should handle going to a label if less than zero", () => {
+    let parser = new Parser(`\n\t\t\t\t\n`);
+    parser.stack = [4];
+    parser.labels = { "\t\t": 130 };
+
+    parser.parseCommand();
+    expect(parser.position).toBe(6);
+    expect(parser.calls).toEqual([]);
+    expect(parser.stack).toEqual([]);
+
+    parser = new Parser(`\n\t\t\t\t\n`);
+    parser.stack = [-3];
+    parser.labels = { "\t\t": 130 };
+
+    parser.parseCommand();
+    expect(parser.position).toBe(130);
+    expect(parser.calls).toEqual([]);
+    expect(parser.stack).toEqual([]);
+  });
+  it("should handle leaving a subroutine", () => {
+    let parser = new Parser(`\n\t\n`);
+    parser.calls = [1000];
+
+    parser.parseCommand();
+    expect(parser.position).toBe(1000);
+    expect(parser.calls).toEqual([]);
+    expect(parser.stack).toEqual([]);
+  });
+  it("should handle leaving the program", () => {
+    let parser = new Parser(`\n\n\n`);
+    parser.calls = [1000];
+
+    parser.parseCommand();
+    expect(parser.exit).toBe(true);
+  });
+});
+
+describe.only("whitespace", () => {
+  it("should correctly outpu", () => {
+    var outputA = "   \t     \t\n\t\n  \n\n\n";
+
+    expect(Parser.whitespace(outputA)).toBe("A");
+  });
+  it("should work", () => {
+    var pushTwice = "   \t\t\n   \t\t\n\t\n \t\t\n \t\n\n\n";
+    var duplicate = "   \t\t\n \n \t\n \t\t\n \t\n\n\n";
+    var duplicateN1 = "   \t\n   \t \n   \t\t\n \t  \t \n\t\n \t\n\n\n";
+    var duplicateN2 = "   \t\n   \t \n   \t\t\n \t  \t\n\t\n \t\n\n\n";
+    var duplicateN3 = "   \t\n   \t \n   \t\t\n \t   \n\t\n \t\n\n\n";
+    var swap = "   \t\t\n   \t \n \n\t\t\n \t\t\n \t\n\n\n";
+    var discard = "   \t\t\n   \t \n \n\t \n\n\t\n \t\n\n\n";
+    var slide =
+      "   \t\t\n   \t \n   \t\n   \t  \n   \t\t \n   \t \t\n   \t\t\t\n \n\t \t\n \t\t\n\t\n \t\t\n \t\t\n \t\t\n \t\n\n\n";
+
+    expect(Parser.whitespace(pushTwice)).toEqual("33");
+    expect(Parser.whitespace(duplicate)).toEqual("33");
+    expect(Parser.whitespace(duplicateN1)).toEqual("1");
+    expect(Parser.whitespace(duplicateN2)).toEqual("2");
+    expect(Parser.whitespace(duplicateN3)).toEqual("3");
+    expect(Parser.whitespace(swap)).toEqual("32");
+    expect(Parser.whitespace(discard)).toEqual("2");
+    expect(Parser.whitespace(slide)).toEqual("5123");
+  });
+});
